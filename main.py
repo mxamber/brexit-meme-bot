a788b29145e080e6a3302fceab61b91f9a09cc14
Amber <13611718+mxamber@users.noreply.github.com>
2019-11-12 22:54:40 +0100

    adapted to desktop use
    removed discord-related code. fixed issue with malpositioned text. allowed for custom url text. allowing text and filename input via command line.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import PIL.ImageFont
 import PIL.ImageDraw
 import PIL.Image
-import discord.ext.commands
 import logging
 import io
 
@@ -14,7 +13,7 @@ font_s = PIL.ImageFont.truetype("font2.ttf", 50)
 blue = (26,52,84)
 red = (218,64,50)
 
-def draw_text(text):
+def draw_text(text, text2):
   img = PIL.Image.open("blank.png")
   draw = PIL.ImageDraw.Draw(img)
 
@@ -30,37 +29,31 @@ def draw_text(text):
   posx += size[0]
 
   draw.text((posx, posy)," for", blue, font=font)
-  posy += size[1]
+  posy += size[1] - 150; # i added the -150
 
   posx = 240
   draw.text((posx, posy), text.title(), blue, font=font)
 
   posx = 430
-  posy = 835
+  posy = 850 # originally 835
 
   size = draw.textsize(f"Get ready for {text.title()} at ", font=font_s)
   draw.text((posx, posy),f"Get ready for {text.title()} at ", blue, font=font_s)
   posx += size[0]
 
-  size = draw.textsize(f"gov.uk/{text.lower().replace(' ', '-')}", font=font_s)
-  draw.text((posx, posy), f"gov.uk/{text.lower().replace(' ', '-')}", red, font=font_s)
+  size = draw.textsize(f"gov.uk/{text2.lower().replace(' ', '-')}", font=font_s)
+  draw.text((posx, posy), f"gov.uk/{text2.lower().replace(' ', '-')}", red, font=font_s)
 
   draw.line((posx, posy + size[1] + 10, posx + size[0], posy + size[1] + 10), fill=red, width=7)
 
   return img
 
-bot = discord.ext.commands.Bot(command_prefix='$')
-
-@bot.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
-
-@bot.command()
-async def brexit(ctx, *, text):
-    img = draw_text(text)
-    img_b = io.BytesIO()
-    img.save(img_b, format='PNG')
-    img_b.seek(0)
-    await ctx.send(file=discord.File(img_b, filename="brexit.png"))
-
-bot.run('NjM2Mjg5NTQ2OTIwOTE5MDQ1.Xa9c5g.M7KB_sRuwoO2o-9Njaulrn8mVIg')
+text = input("Please enter text: ")
+text2 = input("URL (optional, leave blank): ")
+if (text2.isspace()) == True or (not text2):
+  text2 = text
+  
+img = draw_text(text, text2)
+filename = input("File name (no extensions): ")
+filename = filename + ".png"
+img.save(filename, "PNG")
